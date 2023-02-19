49f0128cb4c5ce261235023d391be72e0f834e31
hwang-db <78591348+hwang-db@users.noreply.github.com>
2023-02-19 19:54:01 +0800

    update var names

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ variable "location" {
 provider "azurerm" {
   features {}
 
-  client_id       = "b1c6d89f-55f7-40f3-93cd-033f47e1716a"
+  client_id       = "ebfcf045-2a84-4a16-bca9-494e08ac029c"
   client_secret   = var.client_secret
   tenant_id       = "34f1bb02-ac1d-4c04-b051-58e094fa668c"
   subscription_id = "3e551f82-c624-4a3e-b8f6-e050a96a7004"
