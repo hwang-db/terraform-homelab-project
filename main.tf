a09c49c462feeda0e03e7bbe672337a7b244a1f3
hwang-db <78591348+hwang-db@users.noreply.github.com>
2023-02-19 19:16:46 +0800

    update var names

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "rg_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
 provider "azurerm" {
   features {}
   client_id       = var.client_id
